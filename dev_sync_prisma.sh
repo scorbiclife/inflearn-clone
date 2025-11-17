cf285d45c776558ef946209d3fac92adbc1ef942
scorbiclife <scorbiclife+git@gmail.com>
2025-11-17 10:55:23 +0900

    feat(be): use js client for prisma

--- dev_sync_prisma.sh
+++ dev_sync_prisma.sh
@@ -7,11 +7,7 @@ popd || exit
 
 # Frontend
 mkdir -p frontend/prisma
-cat backend/prisma/schema.prisma |
-  sed \
-    -e 's/prisma-client/prisma-client-js/' \
-    -e '/\.\.\/generated\/prisma/d' \
-    > frontend/prisma/schema.prisma
+cp backend/prisma/schema.prisma frontend/prisma
 pushd frontend || exit
 pnpm prisma generate
 popd || exit
