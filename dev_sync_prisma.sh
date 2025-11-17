6fc6a517af64c7da861c64eff239bb5fa4252dc3
scorbiclife <scorbiclife+git@gmail.com>
2025-11-17 09:57:19 +0900

    feat(ops): automate backend prisma sync

--- dev_sync_prisma.sh
+++ dev_sync_prisma.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+# Backend
+pushd backend || exit
+pnpm prisma generate && pnpm prisma db push
+popd || exit
+
+# Frontend
 mkdir -p frontend/prisma
 cat backend/prisma/schema.prisma |
   sed \
     -e 's/prisma-client/prisma-client-js/' \
     -e '/\.\.\/generated\/prisma/d' \
     > frontend/prisma/schema.prisma
-cd frontend
+pushd frontend || exit
 pnpm prisma generate
+popd || exit
