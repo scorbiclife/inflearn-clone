3d733e271f5628b5ab27f668aa9ff49241050125
scorbiclife <scorbiclife+git@gmail.com>
2025-11-22 06:40:59 +0900

    feat(be): add category endpoint

---
+++ backend/src/category/category.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+  async findAll() {
+    return await this.prisma.courseCategory.findMany();
+  }
+}
